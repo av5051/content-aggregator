b8a74db0922d97616bc93483cf849d008fcaf887
Arpit2006 <69390380+Arpit2006@users.noreply.github.com>
2020-09-30 11:45:00 +0530

    this is a project that tells you the news by voice

---
+++ scraper.py
@@ -0,0 +1,38 @@
+#api key = fbcdee4455a849be9cec887d8b70d2ed
+
+# importing requests package 
+import requests	 
+
+def GetNews(): 
+	
+	# BBC news api 
+	main_url = "http://newsapi.org/v2/everything?q=India&from=2020-08-13&sortBy=publishedAt&apiKey=fbcdee4455a849be9cec887d8b70d2ed"
+
+	# fetching data in json format 
+	open_bbc_page = requests.get(main_url).json() 
+
+	# getting all articles in a string article 
+	article = open_bbc_page["articles"] 
+
+	# empty list which will 
+	# contain all trending news 
+	results = [] 
+	
+	for ar in article: 
+		results.append(ar["title"]) 
+		
+	for i in range(len(results)): 
+		
+		# printing all trending news 
+		print(i + 1, results[i]) 
+
+	#to read the news out loud for us 
+	from win32com.client import Dispatch 
+	speak = Dispatch("SAPI.Spvoice") 
+	speak.Speak(results)				 
+
+# Driver Code 
+if __name__ == '__main__': 
+	
+	# function call 
+	GetNews() 
